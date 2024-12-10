0791056835bec89717aec0e98cb63fb8f038d7ec
Zeynep Çırçır <zynpcrcr@gmail.com>
2024-12-10 18:45:46 +0300

    Add responsive header with assets and routes integration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './layout/header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full',
+    },
+];
