c0031a705ac5ebe0ce2429319b13d188a817833d
Zeynep Çırçır <zynpcrcr@gmail.com>
2024-12-10 18:39:25 +0300

    feat(header): add responsive header component with icons and logo

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
+
+
