b563246e896a842f42e203f8d97e0d1622709626
Zeynep Çırçır <zynpcrcr@gmail.com>
2024-12-11 12:52:44 +0300

    added main component structure

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { SensorMapComponent } from '../../modules/sensor-map/sensor-map.component';
+import { DevicesComponent } from '../../shared/devices/devices.component';
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [SensorMapComponent, DevicesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Kullanacağınız dosya uzantılarını ekleyin
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
